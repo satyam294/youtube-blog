4c9b75a5be12a1da254b2e037ee8f0cbd16ec3f8
satyam294 <satyamchand294@gmail.com>
2026-09-21 22:33:30 +0530

    fix: make 'attachUser' middleware non-fatal

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,14 +1,31 @@
 const { verifyToken } = require("../services/authentication");
+const config = require("../config/index");
 
 function attachUserIfPresent(cookieName) {
   return function(req, res, next) {
     const token = req.cookies[cookieName];
     if(!token) return next();
 
-    const userPayload = verifyToken(token);
-    req.user = userPayload;
+    try {
+      const userPayload = verifyToken(token);
+      req.user = userPayload;
+      next();
+    } catch(err) {
+      req.user = undefined;
+      // Remove the invalid cookie from the browser.
+      res.clearCookie("sessionToken", config.COOKIE_OPTIONS);
 
-    next();
+      // Log the actual reason on the server.
+      if (err.name === "TokenExpiredError") {
+        console.log("Expired authentication token");
+      } else if (err.name === "JsonWebTokenError") {
+        console.log("Invalid/tampered authentication token");
+      } else {
+        console.log("Authentication token verification failed:", err.message);
+      }
+      
+      return next();
+    }
   }
 }
 
